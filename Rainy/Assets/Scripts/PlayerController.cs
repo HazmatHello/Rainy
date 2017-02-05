4471081ff845b632d96e84ba9ad99d27eb10d55e
HazmatHello <engle.garrett@gmail.com>
2017-02-04 21:06:25 -0500

    test1

---
+++ Rainy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public float maxSpeed = 10f;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * maxSpeed;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * maxSpeed;
+
+        transform.Rotate(0, 0, 0);
+        transform.Translate(x, y, 0);
+        Debug.Log("moving");
+    }
+}
